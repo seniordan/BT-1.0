4c62036584b78e9f4c34fbd0255893f68a518e38
ChipSea <chipsea@ChipSeadeMacBook-Pro.local>
2014-09-19 15:34:09 +0800

    Initial Commit

---
+++ BT-1.0Tests/BT_1_0Tests.m
@@ -0,0 +1,34 @@
+//
+//  BT_1_0Tests.m
+//  BT-1.0Tests
+//
+//  Created by ChipSea on 14-9-19.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BT_1_0Tests : XCTestCase
+
+@end
+
+@implementation BT_1_0Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
